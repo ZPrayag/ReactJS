ba0c6ad2fadc9939665c7f6291b4407efe193fdd
Prayag Agrawal <prayag.a@PRAYAGAGRAWAL-408-AIR.local>
2022-08-19 23:02:41 +0530

    Added button component

---
+++ react-task-tracker/src/components/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+const Button = ({ color, text }) => {
+  return (
+    <button style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelBlue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+// ÷const PropTypes
+
+export default Button;
