de0defae9797f91a8cce7bc8153ff1ce34a4d48f
Prayag Agrawal <prayag.a@PRAYAGAGRAWAL-408-AIR.local>
2022-08-25 21:34:08 +0530

    Modified props

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,10 +1,14 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
       <h3>
-        {task.text} <FaTimes style={{ color: "red", curson: "pointer" }} />
+        {task.text}{" "}
+        <FaTimes
+          style={{ color: "red", curson: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>{task.day}</p>
     </div>
